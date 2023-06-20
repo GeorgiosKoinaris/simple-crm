027ca12a5c168bbda6fffd259af7ff944cbe7626
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-06-20 15:21:01 +0200

    first try for recall document id

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  doc,
+  docData,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.scss']
+  styleUrls: ['./user-detail.component.scss'],
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+  firestore: Firestore = inject(Firestore);
+  users$: Observable<any[]>;
+  userId = '';
 
+  constructor(private route: ActivatedRoute) {
+    const aCollection = collection(this.firestore, 'user', this.userId);
+    this.users$ = collectionData(aCollection);
+  }
+
+  ngOnInit(): void {
+    this.userId = this.route.snapshot.params['id'];
+    console.log('GOT ID', this.userId);
+    this.getUser();
+  }
+
+  getUser() {
+    const userRef: any = doc(this.firestore, 'user', this.userId);
+    this.users$ = docData(userRef);
+    console.log('Current User is', this.users$);
+  }
 }
